6c77826a2b048725e8cdd58a098783b449774dfb
Yuri Volchkov <yuri@volch.org>
2022-08-10 13:44:58 +0000

    update proto data
    Signed-off-by: Yuri Volchkov <yuri@volch.org>

--- api/python/config/storage_pb2.py
+++ api/python/config/storage_pb2.py
@@ -23,7 +23,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto3',
   serialized_options=b'\n\025org.lfedge.eve.configZ$github.com/lf-edge/eve/api/go/config',
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n\x14\x63onfig/storage.proto\x12\x15org.lfedge.eve.config\x1a\x16\x63onfig/devcommon.proto\x1a\x18\x63onfig/acipherinfo.proto\x1a\x19\x65vecommon/evecommon.proto\"P\n\rSignatureInfo\x12\x15\n\rintercertsurl\x18\x01 \x01(\t\x12\x15\n\rsignercerturl\x18\x02 \x01(\t\x12\x11\n\tsignature\x18\x03 \x01(\x0c\"\xe5\x01\n\x0f\x44\x61tastoreConfig\x12\n\n\x02id\x18\x64 \x01(\t\x12,\n\x05\x64Type\x18\x01 \x01(\x0e\x32\x1d.org.lfedge.eve.config.DsType\x12\x0c\n\x04\x66qdn\x18\x02 \x01(\t\x12\x0e\n\x06\x61piKey\x18\x03 \x01(\t\x12\x10\n\x08password\x18\x04 \x01(\t\x12\r\n\x05\x64path\x18\x05 \x01(\t\x12\x0e\n\x06region\x18\x06 \x01(\t\x12\x36\n\ncipherData\x18\x07 \x01(\x0b\x32\".org.lfedge.eve.config.CipherBlock\x12\x11\n\tdsCertPEM\x18\x08 \x03(\x0c\"\xec\x01\n\x05Image\x12=\n\x0euuidandversion\x18\x01 \x01(\x0b\x32%.org.lfedge.eve.config.UUIDandVersion\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x0e\n\x06sha256\x18\x03 \x01(\t\x12.\n\x07iformat\x18\x04 \x01(\x0e\x32\x1d.org.lfedge.eve.config.Format\x12\x35\n\x07siginfo\x18\x05 \x01(\x0b\x32$.org.lfedge.eve.config.SignatureInfo\x12\x0c\n\x04\x64sId\x18\x06 \x01(\t\x12\x11\n\tsizeBytes\x18\x08 \x01(\x03\"\xd0\x01\n\x05\x44rive\x12+\n\x05image\x18\x01 \x01(\x0b\x32\x1c.org.lfedge.eve.config.Image\x12\x10\n\x08readonly\x18\x05 \x01(\x08\x12\x10\n\x08preserve\x18\x06 \x01(\x08\x12\x31\n\x07\x64rvtype\x18\x08 \x01(\x0e\x32 .org.lfedge.eve.config.DriveType\x12-\n\x06target\x18\t \x01(\x0e\x32\x1d.org.lfedge.eve.config.Target\x12\x14\n\x0cmaxsizebytes\x18\n \x01(\x03\"\xf2\x01\n\x0b\x43ontentTree\x12\x0c\n\x04uuid\x18\x01 \x01(\t\x12\x0c\n\x04\x64sId\x18\x02 \x01(\t\x12\x0b\n\x03URL\x18\x03 \x01(\t\x12.\n\x07iformat\x18\x04 \x01(\x0e\x32\x1d.org.lfedge.eve.config.Format\x12\x0e\n\x06sha256\x18\x05 \x01(\t\x12\x14\n\x0cmaxSizeBytes\x18\x06 \x01(\x04\x12\x35\n\x07siginfo\x18\x07 \x01(\x0b\x32$.org.lfedge.eve.config.SignatureInfo\x12\x13\n\x0b\x64isplayName\x18\x08 \x01(\t\x12\x18\n\x10generation_count\x18\t \x01(\x03\"r\n\x13VolumeContentOrigin\x12<\n\x04type\x18\x01 \x01(\x0e\x32..org.lfedge.eve.config.VolumeContentOriginType\x12\x1d\n\x15\x64ownloadContentTreeID\x18\x02 \x01(\t\"\xad\x02\n\x06Volume\x12\x0c\n\x04uuid\x18\x01 \x01(\t\x12:\n\x06origin\x18\x02 \x01(\x0b\x32*.org.lfedge.eve.config.VolumeContentOrigin\x12?\n\tprotocols\x18\x03 \x03(\x0e\x32,.org.lfedge.eve.config.VolumeAccessProtocols\x12\x17\n\x0fgenerationCount\x18\x04 \x01(\x03\x12\x14\n\x0cmaxsizebytes\x18\x05 \x01(\x03\x12\x10\n\x08readonly\x18\x06 \x01(\x08\x12\x13\n\x0b\x64isplayName\x18\x07 \x01(\t\x12\x12\n\nclear_text\x18\x08 \x01(\x08\x12.\n\x07iformat\x18\t \x01(\x0e\x32\x1d.org.lfedge.eve.config.Format\"\xb8\x01\n\nDiskConfig\x12\x34\n\x04\x64isk\x18\x01 \x01(\x0b\x32&.org.lfedge.eve.common.DiskDescription\x12\x38\n\x08old_disk\x18\x02 \x01(\x0b\x32&.org.lfedge.eve.common.DiskDescription\x12:\n\x0b\x64isk_config\x18\x03 \x01(\x0e\x32%.org.lfedge.eve.config.DiskConfigType\"\xb0\x01\n\x0b\x44isksConfig\x12\x30\n\x05\x64isks\x18\x01 \x03(\x0b\x32!.org.lfedge.eve.config.DiskConfig\x12\x39\n\narray_type\x18\x02 \x01(\x0e\x32%.org.lfedge.eve.config.DisksArrayType\x12\x34\n\x08\x63hildren\x18\x03 \x03(\x0b\x32\".org.lfedge.eve.config.DisksConfig*\x85\x01\n\x06\x44sType\x12\r\n\tDsUnknown\x10\x00\x12\n\n\x06\x44sHttp\x10\x01\x12\x0b\n\x07\x44sHttps\x10\x02\x12\x08\n\x04\x44sS3\x10\x03\x12\n\n\x06\x44sSFTP\x10\x04\x12\x17\n\x13\x44sContainerRegistry\x10\x05\x12\x0f\n\x0b\x44sAzureBlob\x10\x06\x12\x13\n\x0f\x44sGoogleStorage\x10\x07*~\n\x06\x46ormat\x12\x0e\n\nFmtUnknown\x10\x00\x12\x07\n\x03RAW\x10\x01\x12\x08\n\x04QCOW\x10\x02\x12\t\n\x05QCOW2\x10\x03\x12\x07\n\x03VHD\x10\x04\x12\x08\n\x04VMDK\x10\x05\x12\x07\n\x03OVA\x10\x06\x12\x08\n\x04VHDX\x10\x07\x12\r\n\tCONTAINER\x10\x08\x12\x11\n\rAppCustomBlob\x10\t*G\n\x06Target\x12\x0e\n\nTgtUnknown\x10\x00\x12\x08\n\x04\x44isk\x10\x01\x12\n\n\x06Kernel\x10\x02\x12\n\n\x06Initrd\x10\x03\x12\x0b\n\x07RamDisk\x10\x04*I\n\tDriveType\x12\x10\n\x0cUnclassified\x10\x00\x12\t\n\x05\x43\x44ROM\x10\x01\x12\x07\n\x03HDD\x10\x02\x12\x07\n\x03NET\x10\x03\x12\r\n\tHDD_EMPTY\x10\x04*1\n\x15VolumeAccessProtocols\x12\x0c\n\x08VAP_NONE\x10\x00\x12\n\n\x06VAP_9P\x10\x01*N\n\x17VolumeContentOriginType\x12\x10\n\x0cVCOT_UNKNOWN\x10\x00\x12\x0e\n\nVCOT_BLANK\x10\x01\x12\x11\n\rVCOT_DOWNLOAD\x10\x02*\xec\x01\n\x0e\x44iskConfigType\x12 \n\x1c\x44ISK_CONFIG_TYPE_UNSPECIFIED\x10\x00\x12\x1a\n\x16\x44ISK_CONFIG_TYPE_EVEOS\x10\x01\x12\x1c\n\x18\x44ISK_CONFIG_TYPE_PERSIST\x10\x02\x12\x1f\n\x1b\x44ISK_CONFIG_TYPE_ZFS_ONLINE\x10\x03\x12 \n\x1c\x44ISK_CONFIG_TYPE_ZFS_OFFLINE\x10\x04\x12\x1e\n\x1a\x44ISK_CONFIG_TYPE_APPDIRECT\x10\x05\x12\x1b\n\x17\x44ISK_CONFIG_TYPE_UNUSED\x10\x06*\xa2\x01\n\x0e\x44isksArrayType\x12 \n\x1c\x44ISKS_ARRAY_TYPE_UNSPECIFIED\x10\x00\x12\x1a\n\x16\x44ISKS_ARRAY_TYPE_RAID0\x10\x01\x12\x1a\n\x16\x44ISKS_ARRAY_TYPE_RAID1\x10\x02\x12\x1a\n\x16\x44ISKS_ARRAY_TYPE_RAID5\x10\x03\x12\x1a\n\x16\x44ISKS_ARRAY_TYPE_RAID6\x10\x04\x42=\n\x15org.lfedge.eve.configZ$github.com/lf-edge/eve/api/go/configb\x06proto3'
+  serialized_pb=b'\n\x14\x63onfig/storage.proto\x12\x15org.lfedge.eve.config\x1a\x16\x63onfig/devcommon.proto\x1a\x18\x63onfig/acipherinfo.proto\x1a\x19\x65vecommon/evecommon.proto\"P\n\rSignatureInfo\x12\x15\n\rintercertsurl\x18\x01 \x01(\t\x12\x15\n\rsignercerturl\x18\x02 \x01(\t\x12\x11\n\tsignature\x18\x03 \x01(\x0c\"\xe5\x01\n\x0f\x44\x61tastoreConfig\x12\n\n\x02id\x18\x64 \x01(\t\x12,\n\x05\x64Type\x18\x01 \x01(\x0e\x32\x1d.org.lfedge.eve.config.DsType\x12\x0c\n\x04\x66qdn\x18\x02 \x01(\t\x12\x0e\n\x06\x61piKey\x18\x03 \x01(\t\x12\x10\n\x08password\x18\x04 \x01(\t\x12\r\n\x05\x64path\x18\x05 \x01(\t\x12\x0e\n\x06region\x18\x06 \x01(\t\x12\x36\n\ncipherData\x18\x07 \x01(\x0b\x32\".org.lfedge.eve.config.CipherBlock\x12\x11\n\tdsCertPEM\x18\x08 \x03(\x0c\"\xec\x01\n\x05Image\x12=\n\x0euuidandversion\x18\x01 \x01(\x0b\x32%.org.lfedge.eve.config.UUIDandVersion\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x0e\n\x06sha256\x18\x03 \x01(\t\x12.\n\x07iformat\x18\x04 \x01(\x0e\x32\x1d.org.lfedge.eve.config.Format\x12\x35\n\x07siginfo\x18\x05 \x01(\x0b\x32$.org.lfedge.eve.config.SignatureInfo\x12\x0c\n\x04\x64sId\x18\x06 \x01(\t\x12\x11\n\tsizeBytes\x18\x08 \x01(\x03\"\xd0\x01\n\x05\x44rive\x12+\n\x05image\x18\x01 \x01(\x0b\x32\x1c.org.lfedge.eve.config.Image\x12\x10\n\x08readonly\x18\x05 \x01(\x08\x12\x10\n\x08preserve\x18\x06 \x01(\x08\x12\x31\n\x07\x64rvtype\x18\x08 \x01(\x0e\x32 .org.lfedge.eve.config.DriveType\x12-\n\x06target\x18\t \x01(\x0e\x32\x1d.org.lfedge.eve.config.Target\x12\x14\n\x0cmaxsizebytes\x18\n \x01(\x03\"\xf2\x01\n\x0b\x43ontentTree\x12\x0c\n\x04uuid\x18\x01 \x01(\t\x12\x0c\n\x04\x64sId\x18\x02 \x01(\t\x12\x0b\n\x03URL\x18\x03 \x01(\t\x12.\n\x07iformat\x18\x04 \x01(\x0e\x32\x1d.org.lfedge.eve.config.Format\x12\x0e\n\x06sha256\x18\x05 \x01(\t\x12\x14\n\x0cmaxSizeBytes\x18\x06 \x01(\x04\x12\x35\n\x07siginfo\x18\x07 \x01(\x0b\x32$.org.lfedge.eve.config.SignatureInfo\x12\x13\n\x0b\x64isplayName\x18\x08 \x01(\t\x12\x18\n\x10generation_count\x18\t \x01(\x03\"r\n\x13VolumeContentOrigin\x12<\n\x04type\x18\x01 \x01(\x0e\x32..org.lfedge.eve.config.VolumeContentOriginType\x12\x1d\n\x15\x64ownloadContentTreeID\x18\x02 \x01(\t\"\xfd\x01\n\x06Volume\x12\x0c\n\x04uuid\x18\x01 \x01(\t\x12:\n\x06origin\x18\x02 \x01(\x0b\x32*.org.lfedge.eve.config.VolumeContentOrigin\x12?\n\tprotocols\x18\x03 \x03(\x0e\x32,.org.lfedge.eve.config.VolumeAccessProtocols\x12\x17\n\x0fgenerationCount\x18\x04 \x01(\x03\x12\x14\n\x0cmaxsizebytes\x18\x05 \x01(\x03\x12\x10\n\x08readonly\x18\x06 \x01(\x08\x12\x13\n\x0b\x64isplayName\x18\x07 \x01(\t\x12\x12\n\nclear_text\x18\x08 \x01(\x08\"\xb8\x01\n\nDiskConfig\x12\x34\n\x04\x64isk\x18\x01 \x01(\x0b\x32&.org.lfedge.eve.common.DiskDescription\x12\x38\n\x08old_disk\x18\x02 \x01(\x0b\x32&.org.lfedge.eve.common.DiskDescription\x12:\n\x0b\x64isk_config\x18\x03 \x01(\x0e\x32%.org.lfedge.eve.config.DiskConfigType\"\xb0\x01\n\x0b\x44isksConfig\x12\x30\n\x05\x64isks\x18\x01 \x03(\x0b\x32!.org.lfedge.eve.config.DiskConfig\x12\x39\n\narray_type\x18\x02 \x01(\x0e\x32%.org.lfedge.eve.config.DisksArrayType\x12\x34\n\x08\x63hildren\x18\x03 \x03(\x0b\x32\".org.lfedge.eve.config.DisksConfig*\x85\x01\n\x06\x44sType\x12\r\n\tDsUnknown\x10\x00\x12\n\n\x06\x44sHttp\x10\x01\x12\x0b\n\x07\x44sHttps\x10\x02\x12\x08\n\x04\x44sS3\x10\x03\x12\n\n\x06\x44sSFTP\x10\x04\x12\x17\n\x13\x44sContainerRegistry\x10\x05\x12\x0f\n\x0b\x44sAzureBlob\x10\x06\x12\x13\n\x0f\x44sGoogleStorage\x10\x07*~\n\x06\x46ormat\x12\x0e\n\nFmtUnknown\x10\x00\x12\x07\n\x03RAW\x10\x01\x12\x08\n\x04QCOW\x10\x02\x12\t\n\x05QCOW2\x10\x03\x12\x07\n\x03VHD\x10\x04\x12\x08\n\x04VMDK\x10\x05\x12\x07\n\x03OVA\x10\x06\x12\x08\n\x04VHDX\x10\x07\x12\r\n\tCONTAINER\x10\x08\x12\x11\n\rAppCustomBlob\x10\t*G\n\x06Target\x12\x0e\n\nTgtUnknown\x10\x00\x12\x08\n\x04\x44isk\x10\x01\x12\n\n\x06Kernel\x10\x02\x12\n\n\x06Initrd\x10\x03\x12\x0b\n\x07RamDisk\x10\x04*I\n\tDriveType\x12\x10\n\x0cUnclassified\x10\x00\x12\t\n\x05\x43\x44ROM\x10\x01\x12\x07\n\x03HDD\x10\x02\x12\x07\n\x03NET\x10\x03\x12\r\n\tHDD_EMPTY\x10\x04*1\n\x15VolumeAccessProtocols\x12\x0c\n\x08VAP_NONE\x10\x00\x12\n\n\x06VAP_9P\x10\x01*N\n\x17VolumeContentOriginType\x12\x10\n\x0cVCOT_UNKNOWN\x10\x00\x12\x0e\n\nVCOT_BLANK\x10\x01\x12\x11\n\rVCOT_DOWNLOAD\x10\x02*\xec\x01\n\x0e\x44iskConfigType\x12 \n\x1c\x44ISK_CONFIG_TYPE_UNSPECIFIED\x10\x00\x12\x1a\n\x16\x44ISK_CONFIG_TYPE_EVEOS\x10\x01\x12\x1c\n\x18\x44ISK_CONFIG_TYPE_PERSIST\x10\x02\x12\x1f\n\x1b\x44ISK_CONFIG_TYPE_ZFS_ONLINE\x10\x03\x12 \n\x1c\x44ISK_CONFIG_TYPE_ZFS_OFFLINE\x10\x04\x12\x1e\n\x1a\x44ISK_CONFIG_TYPE_APPDIRECT\x10\x05\x12\x1b\n\x17\x44ISK_CONFIG_TYPE_UNUSED\x10\x06*\xa2\x01\n\x0e\x44isksArrayType\x12 \n\x1c\x44ISKS_ARRAY_TYPE_UNSPECIFIED\x10\x00\x12\x1a\n\x16\x44ISKS_ARRAY_TYPE_RAID0\x10\x01\x12\x1a\n\x16\x44ISKS_ARRAY_TYPE_RAID1\x10\x02\x12\x1a\n\x16\x44ISKS_ARRAY_TYPE_RAID5\x10\x03\x12\x1a\n\x16\x44ISKS_ARRAY_TYPE_RAID6\x10\x04\x42=\n\x15org.lfedge.eve.configZ$github.com/lf-edge/eve/api/go/configb\x06proto3'
   ,
   dependencies=[config_dot_devcommon__pb2.DESCRIPTOR,config_dot_acipherinfo__pb2.DESCRIPTOR,evecommon_dot_evecommon__pb2.DESCRIPTOR,])
 
@@ -77,8 +77,8 @@ _DSTYPE = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   serialized_options=None,
-  serialized_start=1920,
-  serialized_end=2053,
+  serialized_start=1872,
+  serialized_end=2005,
 )
 _sym_db.RegisterEnumDescriptor(_DSTYPE)
 
@@ -143,8 +143,8 @@ _FORMAT = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   serialized_options=None,
-  serialized_start=2055,
-  serialized_end=2181,
+  serialized_start=2007,
+  serialized_end=2133,
 )
 _sym_db.RegisterEnumDescriptor(_FORMAT)
 
@@ -184,8 +184,8 @@ _TARGET = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   serialized_options=None,
-  serialized_start=2183,
-  serialized_end=2254,
+  serialized_start=2135,
+  serialized_end=2206,
 )
 _sym_db.RegisterEnumDescriptor(_TARGET)
 
@@ -225,8 +225,8 @@ _DRIVETYPE = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   serialized_options=None,
-  serialized_start=2256,
-  serialized_end=2329,
+  serialized_start=2208,
+  serialized_end=2281,
 )
 _sym_db.RegisterEnumDescriptor(_DRIVETYPE)
 
@@ -251,8 +251,8 @@ _VOLUMEACCESSPROTOCOLS = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   serialized_options=None,
-  serialized_start=2331,
-  serialized_end=2380,
+  serialized_start=2283,
+  serialized_end=2332,
 )
 _sym_db.RegisterEnumDescriptor(_VOLUMEACCESSPROTOCOLS)
 
@@ -282,8 +282,8 @@ _VOLUMECONTENTORIGINTYPE = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   serialized_options=None,
-  serialized_start=2382,
-  serialized_end=2460,
+  serialized_start=2334,
+  serialized_end=2412,
 )
 _sym_db.RegisterEnumDescriptor(_VOLUMECONTENTORIGINTYPE)
 
@@ -333,8 +333,8 @@ _DISKCONFIGTYPE = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   serialized_options=None,
-  serialized_start=2463,
-  serialized_end=2699,
+  serialized_start=2415,
+  serialized_end=2651,
 )
 _sym_db.RegisterEnumDescriptor(_DISKCONFIGTYPE)
 
@@ -374,8 +374,8 @@ _DISKSARRAYTYPE = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   serialized_options=None,
-  serialized_start=2702,
-  serialized_end=2864,
+  serialized_start=2654,
+  serialized_end=2816,
 )
 _sym_db.RegisterEnumDescriptor(_DISKSARRAYTYPE)
 
@@ -894,13 +894,6 @@ _VOLUME = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='iformat', full_name='org.lfedge.eve.config.Volume.iformat', index=8,
-      number=9, type=14, cpp_type=8, label=1,
-      has_default_value=False, default_value=0,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
   ],
   extensions=[
   ],
@@ -914,7 +907,7 @@ _VOLUME = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=1250,
-  serialized_end=1551,
+  serialized_end=1503,
 )
 
 
@@ -959,8 +952,8 @@ _DISKCONFIG = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=1554,
-  serialized_end=1738,
+  serialized_start=1506,
+  serialized_end=1690,
 )
 
 
@@ -1005,8 +998,8 @@ _DISKSCONFIG = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=1741,
-  serialized_end=1917,
+  serialized_start=1693,
+  serialized_end=1869,
 )
 
 _DATASTORECONFIG.fields_by_name['dType'].enum_type = _DSTYPE
@@ -1022,7 +1015,6 @@ _CONTENTTREE.fields_by_name['siginfo'].message_type = _SIGNATUREINFO
 _VOLUMECONTENTORIGIN.fields_by_name['type'].enum_type = _VOLUMECONTENTORIGINTYPE
 _VOLUME.fields_by_name['origin'].message_type = _VOLUMECONTENTORIGIN
 _VOLUME.fields_by_name['protocols'].enum_type = _VOLUMEACCESSPROTOCOLS
-_VOLUME.fields_by_name['iformat'].enum_type = _FORMAT
 _DISKCONFIG.fields_by_name['disk'].message_type = evecommon_dot_evecommon__pb2._DISKDESCRIPTION
 _DISKCONFIG.fields_by_name['old_disk'].message_type = evecommon_dot_evecommon__pb2._DISKDESCRIPTION
 _DISKCONFIG.fields_by_name['disk_config'].enum_type = _DISKCONFIGTYPE
